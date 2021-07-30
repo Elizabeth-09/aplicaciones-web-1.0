2f2fc6c3f0a325dd3475a7ab9ccab596ce22fd35
Elizabeth-09 <2002.k.elizabeth@gmail.com>
2021-07-30 11:17:28 -0600

    Update

---
+++ models/login.php
@@ -0,0 +1,19 @@
+<?php
+     session_start();
+     include '../models/conexion.php';
+     include '../models/login.php';
+     include 'procesos.php';
+
+     if(isset($_POST['acclogin']))
+     {
+          echo $user = $_POST['user'];
+          echo "<br>";
+          echo $passw = $_POST['passw']:
+     }
+
+     else 
+     {
+          header('Location: ../index.php');
+          
+     }
+?>
