e3a042839eefeab61937cf25940f0364692bd7e6
Elizabeth-09 <2002.k.elizabeth@gmail.com>
2021-07-30 10:00:14 -0600

    index del sistema

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!.. CSS>
+    <link rel="stylesheet" href="./public/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="./public/css/boostrap-theme.css">
+
+    <!.. JS ..>
+    <script src="./public/js/jquery-3.5.1.slim.min.js"></script> 
+    <script src="./public/js/jquery-1.9.1.min.js"></script>
+    <script src="./public/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container.fivid">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="contenlogin">
+                    <div class="login">
+                    <h3>Bienvenidos/as</h3>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
