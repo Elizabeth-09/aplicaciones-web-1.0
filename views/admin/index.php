e3d0af4072d5294dbf597ea5832628bddd2cb675
Liseth-Marinero <73726162+Liseth-Marinero@users.noreply.github.com>
2021-08-25 10:42:09 -0600

    actualizacion

--- views/admin/index.php
+++ views/admin/index.php
@@ -22,9 +22,12 @@
         <script src="./public/js/jquery-3.5.1.slim.min.js"></script>
         <script src="./public/js/jquery-1.9.1.min.js"></script>
         <script src="./public/js/bootstrap.min.js"></script>
-        <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
-</head>
+        <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous">
+        crossorigin="anonymous"></script>
+        <script src="../../public/js/funciones-navar.js"></script>
+    </head>
 <body>
+    
     <h3><b>Bienvenido/a: <?php echo $user; ?> </b></h3>
 </body>
 </html>
